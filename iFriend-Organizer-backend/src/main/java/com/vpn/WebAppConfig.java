0db1ee32b087cd7127a1eb89a70cdb4a6a27bd5b
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-06-05 21:58:59 +0530

    feat: Initial setup for Spring Web MVC configuration
    - Added WebAppConfig class with @ComponentScan, @EnableWebMvc, and @Configuration annotations for setting up web application context.
    - Created WebAppInitializer extending AbstractAnnotationConfigDispatcherServletInitializer to initialize DispatcherServlet with WebAppConfig and WebRootConfig.
    - Added WebRootConfig class with @Configuration annotation for root application context configuration.

---
+++ iFriend-Organizer-backend/src/main/java/com/vpn/WebAppConfig.java
@@ -0,0 +1,12 @@
+package com.vpn;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@ComponentScan
+@EnableWebMvc
+@Configuration
+public class WebAppConfig {
+    
+}
---
+++ iFriend-Organizer-backend/src/main/java/com/vpn/WebRootConfig.java
@@ -0,0 +1,8 @@
+package com.vpn;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebRootConfig {
+
+}
