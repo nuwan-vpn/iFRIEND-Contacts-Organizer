ed6c83eb710c5d703edcf14b4eead65ca267ea65
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-06-06 16:53:22 +0530

    Remove the WebAppConfig, WebAppInitializer, and WebRootConfig classes

--- iFriend-Organizer-backend/src/main/java/com/vpn/WebRootConfig.java
+++
@@ -1,8 +0,0 @@
-package com.vpn;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebRootConfig {
-
-}
--- iFriend-Organizer-backend/src/main/java/com/vpn/api/ContactHttpController.java
+++ iFriend-Organizer-backend/src/main/java/com/vpn/api/ContactHttpController.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ap/v1/contacts")
+@RequestMapping("/api/v1/contacts")
 @CrossOrigin
 public class ContactHttpController {
 
